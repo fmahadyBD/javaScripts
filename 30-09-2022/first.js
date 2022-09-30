31889831d3073f9924127e3bfd025a695e5373e8
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-09-30 12:15:12 +0600

    Add files via upload

---
+++ 30-09-2022/first.js
@@ -0,0 +1,45 @@
+// data types
+
+
+console.log("hi fahim");
+//document.write("Hi fahim")
+console.warn("warning");
+console.error("Error");
+// alert("hy")
+// variable
+var a=5;
+var b=5;
+console.log(a+b)
+var s1="Hi baby"
+var s2="This is your"
+console.log(s1);
+console.log(s2);
+// like function
+
+
+var marks={
+    rahi:22,
+    ravi:22
+}
+console.log(marks);
+var vd=undefined;
+// undefined means  you don't store it
+var s;
+console.log(vd);
+console.log(s);
+var s1=null;
+console.log(s1);
+// null means their is nothing in there
+/* NOTE: at the high there is two type of data type
+Primitive data types: Undefinds,null,symble,boolen,Strings,numbers
+non primitive data types: Array,Objects 
+*/
+// array
+var arr=[1,2,3,4,5];
+console.log(arr);
+console.log(arr[1]);
+console.log(arr[0]);
+console.log(arr[3]);
+var arr2=[1,2,3,"Fahim",4,5];
+console.log(arr2);
+console.log(arr2[3]);// it will be print my name
